38a73ad3c57929a0253e255e9bb4bc114d1bd1fc
Sven1047 <svengemperli@gmail.com>
2025-02-22 19:44:28 +0100

    updated Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -7,6 +7,8 @@ windowsize.append(windowsize[0])
 
 screen = pygame.display.set_mode(windowsize)
 
+clock = pygame.time.Clock()
+
 done = False
 black = [0,0,0]
 white = [255,255,255]
@@ -17,7 +19,30 @@ while not done:
     screen.fill(black)
 
     while len(positions) < 1:
-        positions.append([[randint(0,399),randint(0,399)], randint(5000, 8000), [1,0]])
+        direct = randint(1,100)
+        if direct <= 50:
+            positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [0, 0]])
+        elif 50 < direct <=90:
+            direct = randint(1,100)
+            if direct <= 25:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [.01, 0]])
+            elif 25 < direct <= 50:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [0, .01]])
+            elif 50 < direct <= 75:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [- .01, 0]])
+            elif 75 < direct:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [0, -.01]])
+        elif 90 < direct <= 100:
+            direct = randint(1, 100)
+            if direct <= 25:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [.01, .01]])
+            elif 25 < direct <= 50:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [-.01, .01]])
+            elif 50 < direct <= 75:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [.01, -.01]])
+            elif 75 < direct:
+                positions.append([[randint(0, 399), randint(0, 399)], randint(5000, 8000), [-.01, -.01]])
+#        positions.append([[randint(0,399),randint(0,399)], randint(5000, 8000), [.01,0]])
 
     for pos in positions:
         pygame.draw.circle(screen, white, pos[0], 10)
@@ -25,6 +50,8 @@ while not done:
         if pos[1] == 0:
             positions.remove(pos)
             pygame.quit()
+        pos[0][0] = pos[0][0] + pos[2][0]
+        pos[0][1] = pos[0][1] + pos[2][1]
 
     pygame.display.flip()
 
